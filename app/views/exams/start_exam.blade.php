83d1ebd4dbeb664288dcebcb40e72c82aa90ed3e
Dimitar Dimitrov <dimitar032@gmail.com>
2016-03-02 00:42:56 +0200

    Update start_exam.blade.php

--- app/views/exams/start_exam.blade.php
+++ app/views/exams/start_exam.blade.php
@@ -41,4 +41,37 @@
         </body>
 
     </section>
+    
+    <script>
+    $(document).ready(function () {
+        $('#start-button').click(load);
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+    });
+
+    function load() {
+        $('#start-button').hide();
+
+        var getQuizUrl = $(location).attr('href'); //getting the url with a specific quiz
+        $.ajax({
+            method: "POST",
+            url: getQuizUrl
+
+        }).done(function (data) {
+                $('#slickQuiz').slickQuiz({
+                    json: data,
+                    perQuestionResponseMessaging: false,
+                    skipStartButton: true,
+                    preventUnanswered: true
+                });
+            }
+        );
+
+
+    }
+    </script>
 @stop
